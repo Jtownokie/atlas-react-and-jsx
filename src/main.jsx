f8a5ff5b11cf9db0b4e95e922a643862e854663a
Jtownokie <christopher.stephens@atlasschool.com>
2024-08-30 13:37:22 -0500

    Completed task 1, successfully rendered React App

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
 import "./index.css";
+import App from "./App";
+import ReactDOM from "react-dom/client";
 
 // Render the App component to the root element
+const rootDiv = document.getElementById("root");
+const root = ReactDOM.createRoot(rootDiv);
+root.render(<App />);
